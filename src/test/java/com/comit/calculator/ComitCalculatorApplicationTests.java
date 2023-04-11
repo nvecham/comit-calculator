153ed4152664ddd4cc829cfc4056e66a2cf98463
Nikhila Vecham <nvecham@gmail.com>
2023-04-11 15:30:04 -0600

    first commit

---
+++ src/test/java/com/comit/calculator/ComitCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comit.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComitCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
